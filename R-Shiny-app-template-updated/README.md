8bcd582313c9ad639f993aca56c25e153fcd0d72
mtwatso2-eng <55025422+mtwatso2-eng@users.noreply.github.com>
2025-03-11 17:39:43 -0700

    Delete R-Shiny-app-template-updated directory

--- R-Shiny-app-template-updated/README.md
+++
@@ -1,3 +0,0 @@
-# R Shiny App Template
-
-This is a template for an R Shiny app using a custom framework for modules.
--- R-Shiny-app-template-updated/global.R
+++
@@ -1,8 +0,0 @@
-require(shiny)
-require(tidyverse); require(magrittr)
-require(plotly); require(googledrive)
-source("utils.R")
-
-heiferData <- read.csv("Lab03HeiferData.csv")
-
-sapply(list.files(path = "modules", recursive = TRUE, pattern = "^.*\\.R$", full.names = TRUE), source)
